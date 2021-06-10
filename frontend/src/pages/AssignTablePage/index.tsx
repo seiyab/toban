83e00dded4fd12a57a2b326c67486d1de39b610d
seiya <r108338@yahoo.co.jp>
2021-06-10 21:45:09 +0900

    style

--- frontend/src/pages/AssignTablePage/index.tsx
+++ frontend/src/pages/AssignTablePage/index.tsx
@@ -4,10 +4,13 @@ import { createUseStyles } from "react-jss";
 import { AssignTable } from "@/components/AssignTable";
 import { RoleList } from "@/components/RoleList";
 import { NewMemberButton } from "@/components/NewMemberButton";
-import { EmojiSelector, useEmojiSelector } from "@/components/EmojiSelector";
 
 const useStyles = createUseStyles({
   assignTablePage: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  content: {
     display: "flex",
   },
   roleList: {
@@ -17,17 +20,14 @@ const useStyles = createUseStyles({
 
 export const AssignTablePage: React.VoidFunctionComponent = () => {
   const classes = useStyles();
-  const emojiSelectorControl = useEmojiSelector();
   return (
     <main className={classes.assignTablePage}>
-      <AssignTable />
-      <div>
-        <NewMemberButton />
-        <EmojiSelector
-          value={emojiSelectorControl.value}
-          onSelect={emojiSelectorControl.onSelect}
-        />
-        <RoleList className={classes.roleList} />
+      <div className={classes.content}>
+        <AssignTable />
+        <div>
+          <NewMemberButton />
+          <RoleList className={classes.roleList} />
+        </div>
       </div>
     </main>
   );
